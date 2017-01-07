ca3897d48995c61da53009270f13f04f0b633a76
Todd Mohney <toddmohney@gmail.com>
2017-01-07 14:57:15 -0500

    uh, colored. sort of.

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,22 @@
 module Main where
 
 import Control.Monad (sequence_)
+import Data.Monoid ((<>))
+import Data.Text (Text)
 import qualified Data.Text as Text
 import Git (Branch (..), GitError (..))
 import qualified Git
+import System.Console.ANSI as Term
 
 confirmDeleteBranches :: IO ()
 confirmDeleteBranches = do
   bs <- Git.listBranches
-  putStrLn (show (length bs) ++ " branches found.")
+  printInfo (Text.pack $ show (length bs) ++ " branches found.")
   sequence_ $ map askToDelete bs
 
 askToDelete :: Branch -> IO ()
 askToDelete branch = do
-  putStrLn ("Are you sure you want to delete " ++ Text.unpack (getBranchName branch) ++ "? (y/n)")
+  printInfo ("Delete " <> getBranchName branch <> "? (y/n)")
   answer <- getLine
   case answer of
     "y" -> deleteBranch branch
@@ -21,7 +24,7 @@ askToDelete branch = do
 
 askToForceDelete :: Branch -> IO ()
 askToForceDelete branch = do
-  putStrLn ("Would you like to force delete " ++ Text.unpack (getBranchName branch) ++ "? (y/n)")
+  printWarn ("Force delete " <> getBranchName branch <> "? (y/n)")
   answer <- getLine
   case answer of
     "y" -> forceDeleteBranch branch
@@ -31,16 +34,40 @@ deleteBranch :: Branch -> IO ()
 deleteBranch branch = do
   result <- Git.deleteBranch branch
   case result of
-    Right success -> putStrLn $ Text.unpack success
-    Left (NotFullyMergedError msg) -> putStrLn (Text.unpack msg ++ "\n") >> askToForceDelete branch
-    Left (UnhandledError msg) -> putStrLn $ Text.unpack msg
+    Right success -> printInfo success
+    Left (NotFullyMergedError msg) -> printWarn (msg <> "\n") >> askToForceDelete branch
+    Left (UnhandledError msg) -> printError msg
 
 forceDeleteBranch :: Branch -> IO ()
 forceDeleteBranch branch = do
   result <- Git.forceDeleteBranch branch
   case result of
-    Right success -> putStrLn $ Text.unpack success
-    Left gitError -> putStrLn $ show gitError
+    Right success -> printInfo success
+    Left gitError -> printError (Git.getMsg gitError)
+
+printInfo :: Text -> IO ()
+printInfo txt = do
+  setSGR [ SetConsoleIntensity NormalIntensity
+         , SetColor Foreground Vivid Green
+         ]
+  putStrLn (Text.unpack txt)
+  setSGR []
+
+printWarn :: Text -> IO ()
+printWarn txt = do
+  setSGR [ SetConsoleIntensity NormalIntensity
+         , SetColor Foreground Vivid Yellow
+         ]
+  putStrLn (Text.unpack txt)
+  setSGR []
+
+printError :: Text -> IO ()
+printError txt = do
+  setSGR [ SetConsoleIntensity NormalIntensity
+         , SetColor Foreground Vivid Red
+         ]
+  putStrLn (Text.unpack txt)
+  setSGR []
 
 main :: IO ()
 main = confirmDeleteBranches
